724c623d16d140621ce8f92cc07b280b41aef8f0
Sinan AKYAZICI <sinan_akyazici@hotmail.com>
2024-11-30 18:03:01 +0300

    livenessProbe, readinessProbe, added deployment yaml. create main root url, added descriptions yaml files.

--- src/Kube.Service/Program.cs
+++ src/Kube.Service/Program.cs
@@ -1,3 +1,4 @@
+﻿using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 Console.WriteLine(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
 // Add services to the container.
@@ -11,5 +12,29 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", (IEnumerable<EndpointDataSource> endpointSources, HttpContext context) =>
+{
+    var baseUrl = $"{context.Request.Scheme}://{context.Request.Host}";
+
+    var routes = endpointSources
+        .SelectMany(ds => ds.Endpoints)
+        .OfType<RouteEndpoint>()
+        .Select(endpoint => new
+        {
+            HttpMethod = endpoint.Metadata.OfType<HttpMethodMetadata>().FirstOrDefault()?.HttpMethods.FirstOrDefault(),
+            RoutePattern = endpoint.RoutePattern.RawText
+        });
+
+    var list = new StringBuilder();
+    foreach (var route in routes)
+    {
+        list.Append($"{baseUrl}/{route.RoutePattern}".Trim('/'));
+        list.Append(Environment.NewLine);
+    }
+
+    return Results.Content(list.ToString());
+});
+
 
 app.Run();
